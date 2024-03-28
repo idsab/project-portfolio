b691aed50ce271cf9fdba549204240b93e0d48d7
idsab <idasabic@gmail.com>
2024-03-28 21:45:00 +0100

    started styling top section and tech section

--- my-portfolio/src/Components/TechSection/TechSection.jsx
+++ my-portfolio/src/Components/TechSection/TechSection.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import "./TechSection.css"
 
 export const TechSection = () => {
   return (
-    <div>TechSection</div>
+    <div className="TechWrapper">
+      <div className="Techinfo">
+      <h2>Tech:</h2>
+      <p>HTML, CSS, JavaScript, ES6, React, Web Accessibility, API's.</p> 
+      </div>
+      </div>
   )
 }
 
--- my-portfolio/src/Components/TopSection/TopSection.jsx
+++ my-portfolio/src/Components/TopSection/TopSection.jsx
@@ -1,4 +1,5 @@
 import "./TopSection.css";
+import Arrow from "./assets/Arrow.png"
 
 export const TopSection = () => {
   return (
@@ -6,9 +7,14 @@ export const TopSection = () => {
       <div className="intro">
         <h3>Hi, I'm Ida Sabic</h3>
         <h1>Web Developer</h1>
+        <div className="imgtext">
+        <img src="src\Components\TopSection\monkey.jpeg" alt="profile-img" className="ProfileImg" />
         <p> blablablablablabla somethingsomething.</p>
+        </div>
       </div>
-      <img src="src\Components\TopSection\Arrow.png" alt="arrow-icon" className="ArrowIcon" />
-    </div>
+      <div id="arrow-container">
+      <img src={Arrow} alt="arrow-icon" className="ArrowIcon" /></div>
+      </div>
+      
   );
 };
